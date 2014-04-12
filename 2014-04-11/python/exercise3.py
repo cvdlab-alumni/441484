9b519823b669b7accccccf5afda9d35ae583f31d
amirs156 <amirs@hotmail.it>
2014-04-12 02:32:48 +0200

    commit2

--- 2014-04-11/python/exercise3.py
+++ 2014-04-11/python/exercise3.py
@@ -347,9 +347,115 @@ prato= T([1,2])([-80,-70])(prato)
 prato = COLOR([0.482,0.627,0.356])(prato)
 
 
-parc1 = GRID([[28],[30]])
+parc1 = GRID([[30],[40]])
 parc1=T([1,2])([-5,60])(parc1)
 
+# fare le strisce dei parcheggi ...verranno copiate nel secondo parcheggio
 
-completo = STRUCT([solid_model_3D, prato, parc1])
+
+
+pair_x = [T(2)(-120), parc1]
+parc2 = STRUCT(NN(1)(pair_x))
+
+
+
+palazzo1 =GRID([[20],[40],[40]])
+
+finestra1 = GRID([[3],[0],[6]])
+finestra1 = T([1,3])([3.5,30])(finestra1)
+
+
+pair_y = [T(1)(10), finestra1]
+finestra2 = STRUCT(NN(1)(pair_y))
+
+
+finestra12 = STRUCT([finestra1, finestra2])
+finestra12= COLOR([0.501,0,0])(finestra12)
+
+pair_y = [T(3)(-10), finestra12]
+finestre = STRUCT(NN(2)(pair_y))
+
+finestre = STRUCT([finestre, finestra12])
+finestre= COLOR([0.501,0,0])(finestre)
+
+
+pair_yz = [T(2)(40), finestre]
+finestreAltroLato = STRUCT(NN(1)(pair_yz))
+
+pair_xz = R([1,3])(PI/2)(finestre)
+
+pair_xz = [T(1)(60), pair_xz]
+
+finestreLatoLungo = STRUCT(NN(1)(pair_xz))
+finestreLatoLungo = R([1,2])(PI/2)(finestreLatoLungo)
+
+finestreLatoLungo = T([2,3])([-16,17])(finestreLatoLungo)
+
+
+pair_yy = [T(1)(20), finestreLatoLungo]
+finestreLatoLungo2 = STRUCT(NN(1)(pair_yy))
+
+finestre = STRUCT([finestre, finestra12, finestreLatoLungo, finestreLatoLungo2, finestreAltroLato])
+
+
+porta1 =GRID([[7],[0],[9]])
+porta1 = R([1,2])(PI/2)(porta1)
+
+porta1 = T(1)(20)(porta1)
+porta1 = T(2)(17)(porta1)
+
+
+
+palazzo1 = STRUCT([palazzo1, finestre, porta1])
+palazzo1 = T([1,2])([-70,55])(palazzo1)
+
+
+
+viale3 = CUBOID([10,70])
+viale3 = T([1,2])([-25,10])(viale3)
+
+
+viale4 = CUBOID([40,10])
+viale4 = T([1,2])([-55,70])(viale4)
+
+palazzo11 = STRUCT([palazzo1, viale3, viale4])
+
+
+
+palazzo2 = R([1,2])(PI)(palazzo1)
+palazzo2 = STRUCT([T([1,2])([20,30]), palazzo2]) ## questo  utilizzato per fare il palazzo 3e 4
+
+palazzo22 = R([1,2])(PI)(palazzo11)
+palazzo22 = STRUCT([T([1,2])([20,30]), palazzo22]) 
+
+palazzi12 = STRUCT([palazzo11, palazzo22])
+
+viale1 = CUBOID([10,60])
+viale1 = T([1,2])([-50,-50])(viale1)
+
+
+palazzo3 = R([1,2])(-PI/2)(palazzo2)
+palazzo3 = T([1,2])([0,30])(palazzo3)
+palazzo3 = STRUCT([palazzo3, viale1])
+palazzo4 = R([1,2])(PI)(palazzo3)
+palazzo4 = T([1,2])([20,40])(palazzo4)
+
+
+
+parcheggi = STRUCT([parc2, parc1])
+parcheggi = R([1,2])(PI/2)(parcheggi)
+parcheggi = T([1,2])([25,10])(parcheggi)
+
+
+
+viale2 = CUBOID([30,10])
+viale2 = T([1,2])([-35,10])(viale2)
+
+viale5 = CUBOID([80,10])
+viale5 = T([1,2])([-15,58])(viale5)
+
+
+
+
+completo = STRUCT([solid_model_3D, prato, parcheggi, palazzi12, viale1, palazzo3, palazzo4, viale2, viale5])
 VIEW(completo)
